d48d9e5afb22ffa6905604becad3f4b3e6c157fc
sandykolu <sandeep.kolwalkar@gmail.com>
2021-02-28 01:10:58 -0600

    final

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def welcome():
     return (
         f"Hawaii Climate API<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/stations<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/stations<br/>"
         f"/api/v1.0/temp/start/end"
     )
 
@@ -66,12 +66,34 @@ def prec():
     precipitat = {date: prcp for date, prcp in prec}
     return jsonify(precipitat)
 
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017,8,23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).    session.close()
+    results_months = session.query(Measurement.tobs).    filter(Measurement.station == 'USC00519281').    filter(Measurement.date>= prev_year).all()
+    return jsonify(results_months)
+
+
 @app.route("/api/v1.0/stations")
 def stations():
     results = session.query(Station.station).all
     session.close()
     stations=list(np.ravel(results))
     return jsonify(stations)
+
+
+@app.route("/api/v1.0/temp/start/end")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    
+    if not end:
+        results = session.query(*sel).            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    
+    results = session.query(*sel).        filter(Measurement.date >= start).        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
 
 if __name__ == '__main__':
     app.run(debug=True)
